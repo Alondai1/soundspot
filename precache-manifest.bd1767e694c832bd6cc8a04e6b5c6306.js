af8eca58140ae9d38ace1292d22b393e692eb598
Alon Dai <alondai1@gmail.com>
2020-09-20 14:32:43 +0300

    Updates

---
+++ precache-manifest.bd1767e694c832bd6cc8a04e6b5c6306.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853df3706e78d23fa899971815dcb9bb",
+    "url": "/soundspot/index.html"
+  },
+  {
+    "revision": "14464dbecbbbbbaf2da9",
+    "url": "/soundspot/static/css/main.0c5fdd59.chunk.css"
+  },
+  {
+    "revision": "4899826aaf6fa3fdb1a1",
+    "url": "/soundspot/static/js/2.a9874728.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/soundspot/static/js/2.a9874728.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14464dbecbbbbbaf2da9",
+    "url": "/soundspot/static/js/main.7d09c990.chunk.js"
+  },
+  {
+    "revision": "bed251955eaee7554645",
+    "url": "/soundspot/static/js/runtime-main.dd20bc56.js"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/soundspot/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/soundspot/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "980b9f32a3f482b7db688fa8df0488cd",
+    "url": "/soundspot/static/media/Manrope.980b9f32.ttf"
+  },
+  {
+    "revision": "0ee830999e8676ddea89940670f73e1b",
+    "url": "/soundspot/static/media/hero.0ee83099.svg"
+  },
+  {
+    "revision": "c29e542eef213d960d9c0debe37d792c",
+    "url": "/soundspot/static/media/search-24.c29e542e.png"
+  },
+  {
+    "revision": "7c8e0f4afd29d61139f50885284ea662",
+    "url": "/soundspot/static/media/small-logo-black.7c8e0f4a.png"
+  }
+]);
